3e83af314c8ba08c2c9967503b8d237b448e450b
phiunit <26776324+phiunit@users.noreply.github.com>
2023-05-03 18:05:58 -0600

    Update pong_game.js

--- pong_game.js
+++ pong_game.js
@@ -1,166 +1,112 @@
-let paddle;
-let ball;
-let score;
-let chords;
-let chordIndex;
+const paddleHeight = 100;
+const paddleWidth = 20;
+const ballRadius = 10;
+let playerPaddle, ball;
+let gameStarted = false;
+let midiChords = [];
 
 function setup() {
   createCanvas(500, 500);
-  paddle = new Paddle();
+  playerPaddle = new Paddle(height / 2 - paddleHeight / 2);
   ball = new Ball();
-  score = 0;
-  chords = [[60, 64, 67], [65, 69, 72], [67, 71, 74]];
-  chordIndex = 0;
-
-  WebMidi.enable(function (err) {
-    if (err) {
-      console.log("WebMidi could not be enabled.", err);
-    }
+  WebMidi.enable((err) => {
+    if (err) console.log("WebMidi could not be enabled.", err);
   });
 }
 
 function draw() {
   background(0);
-  paddle.show();
-  paddle.update();
-  ball.show();
+  playerPaddle.display();
+  ball.display();
   ball.update();
+  ball.checkPaddleCollision(playerPaddle);
+  checkGameOver();
 
-  if (ball.hits(paddle)) {
-    ball.changeDirection();
-    playChord();
-    score++;
-  }
-
-  textSize(32);
-  fill(255);
-  text("Score: " + score, 10, 30);
-}
-
-function keyPressed() {
-  if (keyCode === UP_ARROW) {
-    paddle.move(-1);
-  } else if (keyCode === DOWN_ARROW) {
-    paddle.move(1);
+  if (!gameStarted && keyIsPressed && key === " ") {
+    gameStarted = true;
+    ball.launch();
   }
 }
 
-function keyReleased() {
-  paddle.move(0);
+class Paddle {
+  constructor(y) {
+    this.y = y;
+  }
+
+  display() {
+    fill(255);
+    rect(width - paddleWidth, this.y, paddleWidth, paddleHeight);
+  }
+}
+
+class Ball {
+  constructor() {
+    this.x = width / 2;
+    this.y = height / 2;
+    this.xSpeed = 0;
+    this.ySpeed = 0;
+  }
+
+  display() {
+    fill(255);
+    ellipse(this.x, this.y, ballRadius * 2);
+  }
+
+  update() {
+    if (gameStarted) {
+      this.x += this.xSpeed;
+      this.y += this.ySpeed;
+
+      if (this.y - ballRadius <= 0 || this.y + ballRadius >= height) {
+        this.ySpeed *= -1;
+      }
+    }
+  }
+
+  launch() {
+    this.xSpeed = -3;
+    this.ySpeed = random(-2, 2);
+  }
+
+  checkPaddleCollision(paddle) {
+    if (this.x + ballRadius >= width - paddleWidth && this.y >= paddle.y && this.y <= paddle.y + paddleHeight) {
+      this.xSpeed *= -1.1;
+      this.ySpeed *= 1.1;
+      playChord();
+    }
+  }
+}
+
+function checkGameOver() {
+  if (ball.x - ballRadius <= 0) {
+    gameStarted = false;
+    ball = new Ball();
+    midiChords = [];
+  }
 }
 
 function playChord() {
-  let output = WebMidi.outputs[0];
-  let chord = chords[chordIndex];
-  output.playChord(chord, "all", 0, 1000);
-
-  chordIndex++;
-  if (chordIndex >= chords.length) {
-    chordIndex = 0;
+  if (WebMidi.enabled) {
+    let output = WebMidi.outputs[0];
+    if (output) {
+      let root = 60 + [0, 5, 7][Math.floor(Math.random() * 3)];
+      let chord = [root, root + 4, root + 7];
+      midiChords.push(chord);
+      output.playChord(chord, 1, {duration: 500});
+    }
   }
 }
 
-function Ball() {
-  // Ball implementation
-}
-
-function Paddle() {
-  // Paddle implementation
-}
-
-function mouseClicked() {
-  if (ball.isOutOfBounds()) {
+function keyPressed() {
+  if (keyCode === UP_ARROW && playerPaddle.y > 0) {
+    playerPaddle.y -= 10;
+  } else if (keyCode === DOWN_ARROW && playerPaddle.y < height - paddleHeight) {
+    playerPaddle.y += 10;
+  } else if (key === 'S') {
     saveMIDIFile();
   }
 }
 
-function saveMIDIFile() {
-  // Save MIDI file implementation
-}
-
-// Add these lines at the beginning of the script
-const MidiWriter = require('midi-writer-js');
-let midiChords = [];
-
-// Ball implementation
-function Ball() {
-  this.x = width / 2;
-  this.y = height / 2;
-  this.r = 10;
-  this.speed = 3;
-  this.xspeed = this.speed;
-  this.yspeed = this.speed;
-
-  this.show = function () {
-    fill(255);
-    ellipse(this.x, this.y, this.r * 2, this.r * 2);
-  };
-
-  this.update = function () {
-    this.x += this.xspeed;
-    this.y += this.yspeed;
-
-    if (this.y > height - this.r || this.y < this.r) {
-      this.yspeed *= -1;
-    }
-
-    if (this.x > width) {
-      this.xspeed *= -1;
-    } else if (this.x < 0) {
-      this.reset();
-    }
-  };
-
-  this.reset = function () {
-    this.x = width / 2;
-    this.y = height / 2;
-    this.xspeed = this.speed;
-    this.yspeed = this.speed;
-  };
-
-  this.hits = function (paddle) {
-    if (this.x - this.r < paddle.x + paddle.w &&
-      this.y > paddle.y &&
-      this.y < paddle.y + paddle.h) {
-      return true;
-    }
-    return false;
-  };
-
-  this.changeDirection = function () {
-    this.xspeed *= -1;
-  };
-
-  this.isOutOfBounds = function () {
-    return this.x < 0;
-  };
-}
-
-// Paddle implementation
-function Paddle() {
-  this.x = 20;
-  this.y = height / 2 - 50;
-  this.w = 20;
-  this.h = 100;
-  this.speed = 5;
-  this.yspeed = 0;
-
-  this.show = function () {
-    fill(255);
-    rect(this.x, this.y, this.w, this.h);
-  };
-
-  this.update = function () {
-    this.y += this.yspeed;
-    this.y = constrain(this.y, 0, height - this.h);
-  };
-
-  this.move = function (dir) {
-    this.yspeed = this.speed * dir;
-  };
-}
-
-// Save MIDI file implementation
 function saveMIDIFile() {
   let data = [];
   let header = "MThd\x00\x00\x00\x06\x00\x01\x00\x01\x01\xE0";
@@ -197,5 +143,3 @@ function saveMIDIFile() {
   link.download = 'PongChords.mid';
   link.click();
 }
-
-
